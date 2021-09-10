779010f5b2c1ecaf47c4b0f1a5a6b083eeadd8e3
tracer-pa-public <tracer@njuics.org>
2021-09-11 02:18:57 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 02:18:57 up 15 days, 15:23,  2 users,  load average: 2.31, 2.49, 2.41 > unitime > 1e0502b613043d4431fc42b98b0b4674531de9f

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -1,17 +1,17 @@
 #include "cpu/cpu.h"
 
-void set_CF_add(uint32_t result, uint32_t src, size_tdata_size) {
+void set_CF_add(uint32_t result, uint32_t src, size_t data_size) {
     result = sign_ext(result & (0xFFFFFFFF >> (32 -data_size)), data_size);
     src= sign_ext(src& (0xFFFFFFFF >> (32 -data_size)), data_size);
     cpu.eflags.CF = result < src; 
 }
 
-void set_ZF(uint32_t result, size_tdata_size) {
+void set_ZF(uint32_t result, size_t data_size) {
     result = result & (0xFFFFFFFF >> (32 -data_size));
     cpu.eflags.ZF= (result == 0);
 }
 
-void set_SF(uint32_t result, size_tdata_size) {
+void set_SF(uint32_t result, size_t data_size) {
     result = sign_ext(result & (0xFFFFFFFF >> (32 -data_size)), data_size);
     cpu.eflags.SF= sign(result);
 }
@@ -29,7 +29,7 @@ void set_PF(uint32_t result){
     return (count % 2) ? 0 : 1;
 }
 
-void set_OF_add(uint32_t result, uint32_t src, uint32_t dest, size_tdata_size) {
+void set_OF_add(uint32_t result, uint32_t src, uint32_t dest, size_t data_size) {
     switch(data_size) {
         case 8:
         result = sign_ext(result & 0xFF, 8);
