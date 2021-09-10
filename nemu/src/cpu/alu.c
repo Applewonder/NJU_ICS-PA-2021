710e6f22938ada513a2e4f2d435184a1fdec5466
tracer-pa-public <tracer@njuics.org>
2021-09-11 02:21:01 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 02:21:01 up 15 days, 15:25,  2 users,  load average: 2.14, 2.39, 2.39 > unitime > a4cceaec1fff4ca860546133f97b0268b09d1db2

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -26,7 +26,7 @@ void set_PF(uint32_t result){
             count++;
         }
     }
-    return (count % 2) ? 0 : 1;
+    cpu.eflags.PF = (count % 2) ? 0 : 1;
 }
 
 void set_OF_add(uint32_t result, uint32_t src, uint32_t dest, size_t data_size) {
