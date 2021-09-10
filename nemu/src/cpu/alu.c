3ca9fd4312d294b0e2962e2c7b1859be57414200
tracer-pa-public <tracer@njuics.org>
2021-09-11 02:17:21 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 02:17:21 up 15 days, 15:21,  2 users,  load average: 2.68, 2.55, 2.42 > unitime > 28f04218a96286fc7b5d5cbcb8d3f766a3407c18

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -1,14 +1,75 @@
 #include "cpu/cpu.h"
 
+void set_CF_add(uint32_t result, uint32_t src, size_tdata_size) {
+    result = sign_ext(result & (0xFFFFFFFF >> (32 -data_size)), data_size);
+    src= sign_ext(src& (0xFFFFFFFF >> (32 -data_size)), data_size);
+    cpu.eflags.CF = result < src; 
+}
+
+void set_ZF(uint32_t result, size_tdata_size) {
+    result = result & (0xFFFFFFFF >> (32 -data_size));
+    cpu.eflags.ZF= (result == 0);
+}
+
+void set_SF(uint32_t result, size_tdata_size) {
+    result = sign_ext(result & (0xFFFFFFFF >> (32 -data_size)), data_size);
+    cpu.eflags.SF= sign(result);
+}
+
+void set_PF(uint32_t result){
+    result = result & (0xFFFFFFFF >> 24);
+    int scan = 1;
+    int count = 0;
+    for(int i = 0; i < 8; i++) {
+        scan << i;
+        if(scan & result) {
+            count++;
+        }
+    }
+    return (count % 2) ? 0 : 1;
+}
+
+void set_OF_add(uint32_t result, uint32_t src, uint32_t dest, size_tdata_size) {
+    switch(data_size) {
+        case 8:
+        result = sign_ext(result & 0xFF, 8);
+        src= sign_ext(src& 0xFF, 8);
+        dest= sign_ext(dest& 0xFF, 8);
+    break;
+        case 16:
+        result = sign_ext(result & 0xFFFF, 16);
+        src= sign_ext(src& 0xFFFF, 16);
+        dest= sign_ext(dest& 0xFFFF, 16);
+    break;
+    default: break;// do nothing
+    }
+    if(sign(src) == sign(dest)) {
+        if(sign(src) != sign(result))
+            cpu.eflags.OF= 1;
+        else
+            cpu.eflags.OF= 0;
+    } 
+    else {
+        cpu.eflags.OF= 0;
+        
+    }
+}
+
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_add(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	uint32_t res = 0;
+	res = src + dest;
+	
+	set_CF_add(res, src, data_size);
+	set_ZF(res, data_size);
+	set_SF(res, data_size);
+	set_PF(res);
+	set_OF_add(res, src, dest, data_size);
+	
+	return res & (0xFFFFFFFF >> (32 -data_size));
 #endif
 }
 
