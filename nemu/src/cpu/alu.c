f7bcd0a119b36ff687dee36aa28d9b5b437a73e2
tracer-pa-public <tracer@njuics.org>
2021-09-11 23:00:23 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:00:23 up 16 days, 12:04,  2 users,  load average: 2.31, 2.27, 2.32 > unitime > e841d6e25af3502ff1aaaea69ee329decfd6baf1

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -67,10 +67,10 @@ void set_CF_adc(uint32_t result, uint32_t src, size_t data_size) {
 	}
 }
 
-void set_CF_sub(uint32_t result, uint32_t src, size_t data_size) {
+void set_CF_sub(uint32_t result, uint32_t dest, size_t data_size) {
     result = sign_ext(result & (0xFFFFFFFF >> (32 -data_size)), data_size);
-    src= sign_ext(src& (0xFFFFFFFF >> (32 -data_size)), data_size);
-    cpu.eflags.CF = result > src;
+    dest = sign_ext(src& (0xFFFFFFFF >> (32 -data_size)), data_size);
+    cpu.eflags.CF = result > dest;
 }
 
 void set_OF_sub(uint32_t result, uint32_t src, uint32_t dest, size_t data_size) {
@@ -134,14 +134,12 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 	return __ref_alu_sub(src, dest, data_size);
 #else
     uint32_t res = 0;
-	
 	res = dest - src;
-	
 	
 	set_PF(res);
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
-	set_CF_sub(res, src, data_size);
+	set_CF_sub(res, dest, data_size);
 	set_OF_sub(res, src, dest, data_size);
 	
 	return res & (0xFFFFFFFF >> (32 -data_size));
