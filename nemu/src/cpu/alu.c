03466708a37a0b921cd836d6097842075658d58c
tracer-pa-public <tracer@njuics.org>
2021-09-11 23:19:04 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:19:04 up 16 days, 12:23,  2 users,  load average: 3.25, 2.65, 2.43 > unitime > f81fcb81d6234eaac6fdafe4212d959b99e2c0cc

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -156,6 +156,8 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 	set_SF(res, data_size);
 	set_PF(res);
 	set_OF_add(res, src, dest, data_size);
+	
+	return res & (0xFFFFFFFF >> (32 -data_size));
 #endif
 }
 
