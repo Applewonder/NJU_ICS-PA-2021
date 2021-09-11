05b75b099dac88c2f8266e11fb1eabf1d81cc41a
tracer-pa-public <tracer@njuics.org>
2021-09-11 22:44:24 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:44:24 up 16 days, 11:48,  2 users,  load average: 2.13, 2.25, 2.36 > unitime > 1d585b5b72b80dd5ff64585d93b8251aa22c390

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -67,6 +67,37 @@ void set_CF_adc(uint32_t result, uint32_t src, size_t data_size) {
 	}
 }
 
+void set_CF_sub(uint32_t result, uint32_t src, size_t data_size) {
+    result = sign_ext(result & (0xFFFFFFFF >> (32 -data_size)), data_size);
+    src= sign_ext(src& (0xFFFFFFFF >> (32 -data_size)), data_size);
+    cpu.eflags.CF = result > src;
+}
+
+void set_OF_sub(uint32_t result, uint32_t src, uint32_t dest, size_t data_size) {
+    switch(data_size) {
+        case 8:
+        result = sign_ext(result & 0xFF, 8);
+        src= sign_ext(src& 0xFF, 8);
+        dest= sign_ext(dest& 0xFF, 8);
+    break;
+        case 16:
+        result = sign_ext(result & 0xFFFF, 16);
+        src= sign_ext(src& 0xFFFF, 16);
+        dest= sign_ext(dest& 0xFFFF, 16);
+    break;
+    default: break;// do nothing
+    }
+    if(sign(src) != sign(dest)) {
+        if(sign(src) != sign(result))
+            cpu.eflags.OF= 1;
+        else
+            cpu.eflags.OF= 0;
+    } 
+    else {
+        cpu.eflags.OF= 0;
+        
+    }
+}
 
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 {
@@ -102,10 +133,17 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sub(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+    uint32_t res = 0;
+	dest = dest ^ (0xFFFFFFFF >> (32 -data_size));
+	res = src + dest;
+	set_PF(res);
+	set_ZF(res, data_size);
+	set_SF(res, data_size);
+	set_CF_sub(res, src, data_size);
+	set_OF_sub(res, src, dest, data_size);
+	
+	return res & (0xFFFFFFFF >> (32 -data_size));
+	
 #endif
 }
 
