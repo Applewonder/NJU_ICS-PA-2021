220c934baa1be9f9532e8d568760196bc14cf696
tracer-pa-public <tracer@njuics.org>
2021-09-11 22:45:27 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:45:27 up 16 days, 11:49,  2 users,  load average: 2.28, 2.29, 2.36 > unitime > 805c355cb71b6d27b75eb5056c0efbb6826bf045

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -135,7 +135,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 #else
     uint32_t res = 0;
 	dest = dest ^ (0xFFFFFFFF >> (32 -data_size));
-	res = src + dest;
+	res = src + dest + 1;
 	set_PF(res);
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
