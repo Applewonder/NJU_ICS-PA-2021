bc4f52d03228d837ff956e73f61b3d3545a96127
tracer-pa-public <tracer@njuics.org>
2021-09-12 21:52:38 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:52:38 up 17 days, 10:56,  2 users,  load average: 1.07, 1.11, 1.21 > unitime > 6012d582d1958a733beb29c4d645a6a7f6ece621

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -241,6 +241,9 @@ int64_t alu_imul(int32_t src, int32_t dest, size_t data_size)
 	return __ref_alu_imul(src, dest, data_size);
 #else
 	int64_t res = 0;
+	int64_t a = sign_ext_64(src, data_size);
+	int64_t b = sign_ext_64(dest, data_size);
+	
 	res = src * dest;
 	
 	return res;
