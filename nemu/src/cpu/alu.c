4106baa7b7c9fed3c5770168303ab622b20d7f30
tracer-pa-public <tracer@njuics.org>
2021-09-12 16:39:06 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:39:06 up 17 days,  5:43,  2 users,  load average: 1.34, 1.64, 1.69 > unitime > e4171c705308eb1e9689b4df6a39e61fa915dbc8

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -354,7 +354,9 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 	return __ref_alu_sar(src, dest, data_size);
 #else
 	uint32_t res = 0;
+	dest = sign_ext(dest, data_size);
 	res = dest >> src;
+	res = res & (0xFFFFFFFF >> (32 -data_size));
 	
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
