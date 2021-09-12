76d1abd28f9515753fd1b7093353cdc4cfb82d57
tracer-pa-public <tracer@njuics.org>
2021-09-12 17:31:38 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:31:38 up 17 days,  6:35,  2 users,  load average: 1.24, 1.14, 1.20 > unitime > 1c7bc15492d1b783a49df8c95d6f71af36eb24b6

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -354,7 +354,7 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 	return __ref_alu_sar(src, dest, data_size);
 #else
 	uint32_t res = 0;
-	tmp = sign_ext(dest, data_size);
+	uint32_t tmp = sign_ext(dest, data_size);
 	if(!(sign(tmp))) {
 	    res = dest >> src;
 	    res = res & (0xFFFFFFFF >> (32 -data_size + src));
