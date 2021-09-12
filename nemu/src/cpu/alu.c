0c03db432f7127ca0115e3fd3da1adbb24248eec
tracer-pa-public <tracer@njuics.org>
2021-09-12 15:46:19 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:46:19 up 17 days,  4:50,  2 users,  load average: 2.80, 1.61, 1.43 > unitime 1631432779093 > c76d31841ecb1918a130c7e43bc6f8c5c8e397f7

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -319,7 +319,7 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size)
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
 	set_PF(res);
-	set_shl_CF(res, dest, data_size);
+	set_shl_CF(src, dest, data_size);
 	
 	return res & (0xFFFFFFFF >> (32 -data_size));
 #endif
