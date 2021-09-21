ad51ab013716b8b98594fb19ba775af71abe28c7
tracer-pa-public <tracer@njuics.org>
2021-09-21 20:02:53 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:02:53 up 26 days,  9:07,  2 users,  load average: 1.05, 1.15, 1.08 > unitime > 13be5d51d74dea2db95a1e761cfd06d35b9e5379

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -22,8 +22,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 		{
 
 			/* TODO: shift right, pay attention to sticky bit*/
-			printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
-			fflush(stdout);
+			
 			assert(0);
 		}
 
