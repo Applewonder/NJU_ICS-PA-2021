afc989e04316204742097fd8e5ef136471880179
tracer-pa-public <tracer@njuics.org>
2021-09-25 12:04:50 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 12:04:50 up 30 days,  1:09,  2 users,  load average: 1.16, 1.16, 1.16 > unitime > 1ae419d94db5414dede8a145964c8c31c2a44b2b

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -218,7 +218,7 @@ uint32_t internal_float_add(uint32_t b, uint32_t a)
 	FLOAT res;
 	res.val = internal_normalize(f.sign, exp_res, sig_res);
 	if(tes != res.fval) {
-	    printf("tes = %f, res = %f", tes, res);
+	    printf("tes = %f, res = %f", tes, res.fval);
 	    printf("fa = %f, fb = %f", fa.fval, fb.fval);
 	    printf("a = %d, b = %d", a, b);
 	    printf("f.sign = %d, exp_res = %d, sig.res = %d", f.sign, exp_res, sig_res);
