255473abd9a2704330c2801ab230ad6bb6596e24
tracer-pa-public <tracer@njuics.org>
2021-10-21 23:32:42 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:32:42 up 56 days, 12:36,  2 users,  load average: 2.34, 2.43, 2.44 > unitime > a1a796e1f3b4b89413215754712ea16dddd8106f

--- nemu/include/cpu/instr/add.h
+++ nemu/include/cpu/instr/add.h
@@ -3,5 +3,10 @@
 /*
 Put the declarations of `add' instructions here.
 */
-
+make_instr_func(add_r2rm_b);
+make_instr_func(add_r2rm_v);
+make_instr_func(add_rm2r_b);
+make_instr_func(add_rm2r_v);
+make_instr_func(add_i2r_b);
+make_instr_func(add_i2r_v);
 #endif
--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -2,3 +2,17 @@
 /*
 Put the implementations of `add' instructions here.
 */
+static void instr_execute_2op() 
+{
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	opr_dest.val = alu_add(opr_src.val, opr_dest.val, data_size);
+	operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(add, r, rm, b)
+make_instr_impl_2op(add, r, rm, v)
+make_instr_impl_2op(add, rm, r, b)
+make_instr_impl_2op(add, rm, r, v)
+make_instr_impl_2op(add, i, r, b)
+make_instr_impl_2op(add, i, r, v)
