70a357acb0ef51fce47a8dab84f414c5a48fca08
tracer-pa-public <tracer@njuics.org>
2021-10-21 19:21:19 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:21:19 up 56 days,  8:25,  2 users,  load average: 2.38, 2.46, 2.40 > unitime > d5b8f877f5e0c132e27ea5b2f83b9e2dc203ab2

--- nemu/include/cpu/instr/adc.h
+++ nemu/include/cpu/instr/adc.h
@@ -4,4 +4,9 @@
 Put the declarations of `adc' instructions here.
 */
 make_instr_func(add_r2rm_b);
+make_instr_func(add_r2rm_v);
+make_instr_func(add_rm2r_b);
+make_instr_func(add_rm2r_v);
+make_instr_func(add_i2r_b);
+make_instr_func(add_i2r_v);
 #endif
--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -6,8 +6,13 @@ static void instr_execute_2op()
 {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
-	opr_dest.val += opr_src.val;
+	opr_dest.val = alu.add(opr_src.val, opr_dest.val, datasize);
 	operand_write(&opr_dest);
 }
 
 make_instr_impl_2op(add, r, rm, b)
+make_instr_impl_2op(add, r, rm, v)
+make_instr_impl_2op(add, rm, r, b)
+make_instr_impl_2op(add, rm, r, v)
+make_instr_impl_2op(add, i, r, b)
+make_instr_impl_2op(add, i, r, v)
