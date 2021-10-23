2d0b5045a3427b942e741435f5c6961264b8de22
tracer-pa-public <tracer@njuics.org>
2021-10-23 16:01:55 +0800

    > test_pa-2-1 > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:01:55 up 58 days,  5:06,  2 users,  load average: 2.28, 2.26, 2.32 > unitime 1634976113913 > 2c8f09bf4c6b928358e3dd2e9a1d0dfec41d0a26

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -6,7 +6,7 @@ int call_near(uint32_t eip, uint8_t opcode)
 {
      int len = 5;
      opr_src.data_size = opr_dest.data_size = 32;
-    
+     data_size = 32;
      cpu.esp -= 4;
      opr_dest.type = OPR_MEM;
      opr_dest.addr = cpu.esp;
