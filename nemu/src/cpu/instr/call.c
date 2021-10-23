a61c44084103d0bfc752de9dc8bfc0e0ae2e5a76
tracer-pa-public <tracer@njuics.org>
2021-10-23 15:56:33 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:56:33 up 58 days,  5:00,  2 users,  load average: 2.10, 2.24, 2.34 > unitime 1634975793640 > a041e440ecff30b7722cc4a8c04b6ab4949cc4e4

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -4,7 +4,7 @@ Put the implementations of `call' instructions here.
 */
 int call_near(uint32_t eip, uint8_t opcode) 
 {
-     int len = 1;
+     int len = 5;
      opr_src.data_size = opr_dest.data_size = 32;
     
      cpu.esp -= 4;
