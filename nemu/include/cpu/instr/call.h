7d80c773ee52ec7f9c6351c1701d5241ca84f925
tracer-pa-public <tracer@njuics.org>
2021-10-23 14:54:23 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:54:23 up 58 days,  3:58,  2 users,  load average: 2.63, 2.41, 2.31 > unitime > 2abcc24bf0c798e0ffda997e2228474c97984b9

--- nemu/include/cpu/instr/call.h
+++ nemu/include/cpu/instr/call.h
@@ -3,5 +3,5 @@
 /*
 Put the declarations of `call' instructions here.
 */
-
+call_near(uint32_t eip, uint8_t opcode);
 #endif
--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -2,3 +2,8 @@
 /*
 Put the implementations of `call' instructions here.
 */
+void call_near(uint32_t eip, uint8_t opcode) 
+{
+    operand_read(&opr_src);
+    cpu.eip += opr_src.val;
+}
