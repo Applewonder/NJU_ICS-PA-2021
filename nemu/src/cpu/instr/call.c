d13ca9cadeefd211fb5d4919a620ce37034066c7
tracer-pa-public <tracer@njuics.org>
2021-10-23 15:49:23 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:49:23 up 58 days,  4:53,  2 users,  load average: 2.68, 2.51, 2.44 > unitime > a151560088d98746ed847ead77ae475b441dfe9

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -4,17 +4,16 @@ Put the implementations of `call' instructions here.
 */
 int call_near(uint32_t eip, uint8_t opcode) 
 {
-    // operand_read(&opr_src);
      int len = 1;
-    // opr_src.data_size = opr_dest.data_size = 32;
+     opr_src.data_size = opr_dest.data_size = 32;
     
-    // cpu.esp -= 4;
-    // opr_dest.type = OPR_MEM;
-    // opr_dest.addr = cpu.esp;
-    // opr_dest.data_size = data_size;
-    // opr_dest.val = cpu.eip;
-    // operand_write(&opr_dest);
+     cpu.esp -= 4;
+     opr_dest.type = OPR_MEM;
+     opr_dest.addr = cpu.esp;
+     opr_dest.data_size = data_size;
+     opr_dest.val = cpu.eip;
+     operand_write(&opr_dest);
 
-    // cpu.eip += opr_src.val;
+     cpu.eip += opr_src.val;
      return len;
 }
