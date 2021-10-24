a788e04bbb24c50c0619afbdf66881513f844250
tracer-pa-public <tracer@njuics.org>
2021-10-24 16:21:05 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:21:05 up 59 days,  5:25,  2 users,  load average: 2.40, 2.32, 2.38 > unitime > d6ce9e655c73a48082c665dada764f3ab48442b5

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -7,8 +7,8 @@ static void instr_execute_2op()
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
 	opr_dest.val = alu_and(opr_src.val, opr_dest.val, data_size);
-	cpu.CF = 0;
-	cpu.OF = 0;
+	cpu.eflags.CF = 0;
+	cpu.eflags.OF = 0;
 	operand_write(&opr_dest);
 }
 make_instr_impl_2op(test, i, rm, v)
