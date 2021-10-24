b4f071323c7d8448e4814e70a23e69e6d76e0559
tracer-pa-public <tracer@njuics.org>
2021-10-24 15:35:01 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:35:01 up 59 days,  4:39,  2 users,  load average: 2.21, 2.39, 2.49 > unitime > 155f52f6a99e9fdd5c5608366cbdfaf673611e1a

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -9,7 +9,7 @@ int ret_near(uint32_t eip, uint8_t opcode)
     opr_dest.addr = cpu.esp;
     opr_dest.data_size = data_size;
     operand_read(&opr_dest);
-    if (data_size == 16) 
+    if (data_size == 16) {
         cpu.eip = opr_dest.val;
         cpu.eip = cpu.eip & 0x0000ffff;
     } else if(data_size == 16) {
