319ef4622b7fe392ce0c879b8182614c9a984e35
tracer-pa-public <tracer@njuics.org>
2021-10-24 15:54:19 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:54:19 up 59 days,  4:58,  2 users,  load average: 2.45, 2.43, 2.41 > unitime > e0b134a430d84974ed5527dbaf55cab923b7517a

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -4,16 +4,12 @@ Put the implementations of `ret' instructions here.
 */
 int ret_near(uint32_t eip, uint8_t opcode)
 {
-    /*opr_dest.type = OPR_MEM;
-    opr_dest.addr = cpu.esp;
-    opr_dest.data_size = data_size;
-    operand_read(&opr_dest);
+    OPERAND opr;
+    opr.type = OPR_MEM;
+    opr.addr = cpu.esp;
+    opr.data_size = data_size;
+    operand_read(&opr);
     cpu.esp += 4;
-    if (data_size == 16) {
-        cpu.eip = opr_dest.val;
-        cpu.eip = cpu.eip & 0x0000ffff;
-    } else if(data_size == 16) {
-        cpu.eip = opr_dest.val;
-    }
+    cpu.eip = opr.val;
     return 0;
 }
