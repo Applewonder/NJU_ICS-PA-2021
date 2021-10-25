8ae1b29ae5847f39460496e52440468d5b594ac3
tracer-pa-public <tracer@njuics.org>
2021-10-25 22:19:32 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:19:32 up 60 days, 11:23,  2 users,  load average: 2.59, 2.44, 2.43 > unitime 1635171572690 > a4e5474c36b88569500678cc842eac2a5221ce1f

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -7,23 +7,23 @@ make_instr_func(pop_r_v)
 {
     int len = 1;
     
-    opcode = instr_fetch(cpu.eip, 1);
-    opcode -= 0x58;
+    // opcode = instr_fetch(cpu.eip, 1);
+    // opcode -= 0x58;
     
-    OPERAND esp;
-    esp.type = OPR_MEM;
-    esp.addr = cpu.esp;
-    esp.data_size = 32;
-    operand_read(&esp);
+    // OPERAND esp;
+    // esp.type = OPR_MEM;
+    // esp.addr = cpu.esp;
+    // esp.data_size = 32;
+    // operand_read(&esp);
     
-    OPERAND opr;
-    opr.type = OPR_REG;
-    opr.addr = opcode;
-    opr.data_size = data_size;
-    opr.val = esp.val;
+    // OPERAND opr;
+    // opr.type = OPR_REG;
+    // opr.addr = opcode;
+    // opr.data_size = data_size;
+    // opr.val = esp.val;
     
-    operand_write(&opr);
-    cpu.esp += 4;
+    // operand_write(&opr);
+    // cpu.esp += 4;
     
     return len;
 }
