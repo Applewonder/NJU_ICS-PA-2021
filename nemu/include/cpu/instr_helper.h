a693dcd798f56de29ed64e7be69032222fbd5981
tracer-pa-public <tracer@njuics.org>
2021-10-25 16:38:20 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:38:20 up 60 days,  5:42,  2 users,  load average: 2.65, 2.39, 2.33 > unitime > ba55b4a96be09816f424e2ea21fd796c28ad3344

--- nemu/include/cpu/instr_helper.h
+++ nemu/include/cpu/instr_helper.h
@@ -179,7 +179,7 @@ static inline bool inv_cc();
 	inv_cc()
 
 #define condition_be \
-	inv_cc()
+	cpu.eflags.CF == 1 || cpu.eflags.ZF == 1
 
 #define condition_o \
 	inv_cc()
@@ -194,7 +194,7 @@ static inline bool inv_cc();
 	cpu.eflags.ZF == 0
 
 #define condition_na \
-	inv_cc()
+	cpu.eflags.CF == 1 || cpu.eflags.ZF == 1
 
 #define condition_no \
 	inv_cc()
