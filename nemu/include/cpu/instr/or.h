b9a9f33da6cf83fb1ffa9ee251eaa6277d3e1316
tracer-pa-public <tracer@njuics.org>
2021-10-26 00:19:30 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 00:19:30 up 60 days, 13:23,  2 users,  load average: 2.29, 2.39, 2.45 > unitime 1635178770087 > 9e027d0d10150fc94b828d74296fc4e6c3ba3828

--- nemu/include/cpu/instr/or.h
+++ nemu/include/cpu/instr/or.h
@@ -3,5 +3,5 @@
 /*
 Put the declarations of `or' instructions here.
 */
-
+make_instr_func(or_r2rm_v);
 #endif
--- nemu/src/cpu/instr/or.c
+++ nemu/src/cpu/instr/or.c
@@ -2,3 +2,12 @@
 /*
 Put the implementations of `or' instructions here.
 */
+static void instr_execute_2op() 
+{
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	
+	opr_dest.val = alu_or(opr_src.val, opr_dest.val, data_size);
+	operand_write(&opr_dest);
+}
+make_instr_impl_2op(or, r, rm, v)
