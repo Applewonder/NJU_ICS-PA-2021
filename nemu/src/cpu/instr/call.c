f489b326a3eb0542ab4ad844d51f1703d41af903
tracer-pa-public <tracer@njuics.org>
2021-10-25 23:10:04 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:10:04 up 60 days, 12:14,  2 users,  load average: 2.67, 2.48, 2.46 > unitime 1635174604148 > ce1bc159a159ab992f25cf2550499ee0388fadd3

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -6,9 +6,10 @@ int call_near(uint32_t eip, uint8_t opcode)
 {
      opr_src.type = OPR_IMM;
      opr_src.addr = cpu.eip + 1;
+     opr_src.data_size = 32;
      operand_read(&opr_src);
      
-     opr_src.data_size = opr_dest.data_size = 32;
+     opr_dest.data_size = 32;
      data_size = 32;
      cpu.esp -= 4;
      opr_dest.type = OPR_MEM;
