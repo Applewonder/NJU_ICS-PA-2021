85c0a4790435a07a2078c4496c4e9e524dc4f6c4
tracer-pa-public <tracer@njuics.org>
2021-10-25 12:06:01 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 12:06:01 up 60 days,  1:10,  2 users,  load average: 2.88, 3.05, 2.82 > unitime > 8a81c82c31de16f71002878f0833c4119d06c59

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -21,7 +21,7 @@ instr_func opcode_entry[256] = {
     /* 0x44 - 0x47*/ inv, inv, inv, inv,
     /* 0x48 - 0x4b*/ inv, inv, inv, inv,
     /* 0x4c - 0x4f*/ inv, inv, inv, inv,
-    /* 0x50 - 0x53*/ inv, inv, inv, inv,
+    /* 0x50 - 0x53*/ inv, inv, inv, push_r_v,
     /* 0x54 - 0x57*/ inv, push_r_v, inv, inv,
     /* 0x58 - 0x5b*/ inv, inv, inv, inv,
     /* 0x5c - 0x5f*/ inv, inv, inv, inv,
