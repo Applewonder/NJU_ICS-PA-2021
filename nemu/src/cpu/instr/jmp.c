40d5f64ba39c891965cc574845dc9ffded115ae8
tracer-pa-public <tracer@njuics.org>
2021-10-25 16:25:34 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:25:34 up 60 days,  5:29,  2 users,  load average: 2.96, 2.64, 2.44 > unitime > 19b389136af2f5dfee28cbc34f2dd1c4ac450838

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -21,12 +21,12 @@ make_instr_func(jmp_near)
 
 make_instr_func(jmp_short)
 {
-        // OPERAND rel;
-        // rel.type = OPR_IMM;
-        // rel.data_size = data_size;
-        // rel.addr = eip + 1;
+        OPERAND rel;
+        rel.type = OPR_IMM;
+        rel.data_size = data_size;
+        rel.addr = cpu.eip + 1;
 
-        // operand_read(&rel);
+        operand_read(&rel);
 
         
 
