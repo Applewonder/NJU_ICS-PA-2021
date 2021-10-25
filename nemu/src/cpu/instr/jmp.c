06893672ea1bb051ba979b8c5cec3291de039551
tracer-pa-public <tracer@njuics.org>
2021-10-25 16:19:17 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:19:17 up 60 days,  5:23,  2 users,  load average: 2.31, 2.32, 2.32 > unitime > f6c204dfe9a85528d37b74b8da678409a296e43b

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -23,17 +23,14 @@ make_instr_func(jmp_short)
 {
         OPERAND rel;
         rel.type = OPR_IMM;
-        rel.sreg = SREG_CS;
         rel.data_size = data_size;
         rel.addr = eip + 1;
 
         operand_read(&rel);
 
-        int offset = sign_ext(rel.val, data_size);
-        // thank Ting Xu from CS'17 for finding this bug
-        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
+        
 
-        cpu.eip += offset;
+        cpu.eip += rel.val;
 
         return 0;
 }
