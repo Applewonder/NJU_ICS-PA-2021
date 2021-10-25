29340eef9dd71d28f134293061f30581d094760a
tracer-pa-public <tracer@njuics.org>
2021-10-25 20:50:19 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:50:19 up 60 days,  9:54,  2 users,  load average: 2.28, 2.46, 2.47 > unitime 1635166219431 > efcc66edcae73130105a52d3bcb1e7fde666df5b

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -7,16 +7,17 @@ static void instr_execute_1op()
     int opcode = instr_fetch(cpu.eip + 1, 1);
     opcode -= 0x58;
     
-    opr_dest.type = OPR_MEM;
-    opr_dest.addr = cpu.esp;
-    opr_dest.data_size = data_size;
-    operand_read(&opr_dest);
+    OPERAND esp;
+    esp.type = OPR_MEM;
+    esp.addr = cpu.esp;
+    esp.data_size = data_size;
+    operand_read(&esp);
     
     OPERAND opr;
     opr.type = OPR_REG;
     opr.addr = opcode;
     opr.data_size = data_size;
-    opr.val = opr_dest.val;
+    opr.val = esp.val;
     
     operand_write(&opr);
     cpu.esp += 4;
