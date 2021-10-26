cb1506844cabd3f7be693673536d7393caa388f0
tracer-pa-public <tracer@njuics.org>
2021-10-26 19:05:46 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:05:46 up 61 days,  8:10,  2 users,  load average: 2.39, 2.41, 2.43 > unitime 1635246346249 > 2d74bd6c39f47e0a97cacbff3fe830f1f95c89c4

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -3,7 +3,7 @@
 instr_func opcode_entry[256] = {
     /* 0x00 - 0x03*/ add_r2rm_b, add_r2rm_v, add_rm2r_b, add_rm2r_v,
     /* 0x04 - 0x07*/ add_i2a_b, add_i2a_v, inv, inv,
-    /* 0x08 - 0x0b*/ inv, __ref_or_r2rm_v, inv, inv,
+    /* 0x08 - 0x0b*/ inv, or_r2rm_v, inv, inv,
     /* 0x0c - 0x0f*/ inv, inv, inv, opcode_2_byte,
     /* 0x10 - 0x13*/ inv, adc_r2rm_v, inv, inv,
     /* 0x14 - 0x17*/ inv, inv, inv, inv,
