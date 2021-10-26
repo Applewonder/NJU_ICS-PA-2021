04cbd5b9e3f344ca77234cc0779cbb22ae2afa6d
tracer-pa-public <tracer@njuics.org>
2021-10-27 00:25:33 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 00:25:33 up 61 days, 13:29,  2 users,  load average: 2.56, 2.45, 2.44 > unitime 1635265533418 > ebf1e411232d76e12ca1846088057bf513b18c8

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -17,21 +17,3 @@ make_instr_impl_2op(test, i, rm, v)
 make_instr_impl_2op(test, i, a, b)
 make_instr_impl_2op(test, i, a, v)
 
-// int test_i2rm_v(uint32_t eip, uint8_t opcode)
-// {
-//     int len = 1;
-//     decode_data_size_v;
-//     decode_operand_i2rm;
-//     operand_read(&opr_src);
-// 	operand_read(&opr_dest);
-// 	opr_dest.val = alu_and(opr_src.val, opr_dest.val, data_size);
-// 	cpu.eflags.CF = 0;
-// 	cpu.eflags.OF = 0;
-// 	operand_write(&opr_dest);
-//     if (data_size == 16) {
-//         len = 4;
-//     }else if(data_size == 32) {
-// 	    len = 6;
-//     }
-//     return len;
-// }
