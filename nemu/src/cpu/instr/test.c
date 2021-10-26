038d359a0aa2a6eeb61c1f16bddf2041a43c9b06
tracer-pa-public <tracer@njuics.org>
2021-10-26 23:51:29 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:51:29 up 61 days, 12:55,  2 users,  load average: 2.18, 2.40, 2.47 > unitime 1635263489470 > fde1a89e2839d03bc4ace746379894b5f02dc9e8

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -22,21 +22,15 @@ int test_i2rm_v(uint32_t eip, uint8_t opcode)
     int len = 1;
     decode_data_size_v;
     decode_operand_i2rm;
+    operand_read(&opr_src);
+	operand_read(&opr_dest);
+	opr_dest.val = alu_and(opr_src.val, opr_dest.val, data_size);
+	cpu.eflags.CF = 0;
+	cpu.eflags.OF = 0;
+	operand_write(&opr_dest);
     if (data_size == 16) {
-        operand_read(&opr_src);
-	    operand_read(&opr_dest);
-	    opr_dest.val = alu_and(opr_src.val, opr_dest.val, data_size);
-	    cpu.eflags.CF = 0;
-	    cpu.eflags.OF = 0;
-	    operand_write(&opr_dest);
 	    return 4;
     }else if(data_size == 32) {
-        operand_read(&opr_src);
-	    operand_read(&opr_dest);
-	    opr_dest.val = alu_and(opr_src.val, opr_dest.val, data_size);
-	    cpu.eflags.CF = 0;
-	    cpu.eflags.OF = 0;
-	    operand_write(&opr_dest);
 	    return 6;
     }
 }}
