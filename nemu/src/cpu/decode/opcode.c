32593fde01157052827783366335c469c9123a18
tracer-pa-public <tracer@njuics.org>
2021-10-26 18:39:54 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:39:54 up 61 days,  7:44,  2 users,  load average: 3.27, 2.61, 2.51 > unitime 1635244794509 > 9409c49a2c9d1474dc0b59e118a7acc3c97adeae

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -77,7 +77,7 @@ instr_func group_1_v_entry[8] =
 
 /* 0x83 */
 instr_func group_1_bv_entry[8] =
-    {__ref_add_i2rm_bv, inv, inv, inv, __ref_and_i2rm_bv, __ref_sub_i2rm_bv, inv, __ref_cmp_i2rm_bv};
+    {__ref_add_i2rm_bv, inv, inv, inv, and_i2rm_bv, __ref_sub_i2rm_bv, inv, __ref_cmp_i2rm_bv};
 
 /* 0xc0 */
 instr_func group_2_b_entry[8] =
