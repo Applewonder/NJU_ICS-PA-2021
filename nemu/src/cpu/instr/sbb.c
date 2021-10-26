ab60fbd75fdac5738ba6b4f7948c086452b509c4
tracer-pa-public <tracer@njuics.org>
2021-10-26 20:21:31 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:21:31 up 61 days,  9:25,  2 users,  load average: 2.95, 2.58, 2.62 > unitime 1635250891438 > 82bdcdb5d6723376d3a953a98116057b9d6fc6a4

--- nemu/src/cpu/instr/sbb.c
+++ nemu/src/cpu/instr/sbb.c
@@ -6,6 +6,6 @@ static void instr_execute_2op()
 {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
-	opr_dest.val = alu_ssb(sign_ext(opr_src.val, opr_src.data_size), opr_dest.val, data_size);
+	opr_dest.val = alu_sbb(sign_ext(opr_src.val, opr_src.data_size), opr_dest.val, data_size);
 	operand_write(&opr_dest);
 }
