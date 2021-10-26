66c392a4a1876e1f53dbfd0094eb7f6d2a7116af
tracer-pa-public <tracer@njuics.org>
2021-10-26 19:53:30 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:53:30 up 61 days,  8:57,  2 users,  load average: 2.37, 2.42, 2.45 > unitime 1635249210352 > 981379c0af7d59af99681b88e799b76d7335ee0

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -14,6 +14,16 @@ make_instr_impl_2op(cmp, i, rm, v)
 make_instr_impl_2op(cmp, rm, r, v)
 make_instr_impl_2op(cmp, rm, r, b)
 make_instr_impl_2op(cmp, r, rm, b)
-make_instr_impl_2op(cmp, i, a, b)
 make_instr_impl_2op(cmp, i, a, v)
 make_instr_impl_2op(cmp, i, rm, b)
+make_instr_func(cmp, i, a, b)
+{
+    int len = 1;
+    decode_data_size_b;
+    decode_operand_a2b;
+    print_asm_2(#inst_name, opr_dest.data_size == 8 ? "b" : (opr_dest.data_size == 16 ? "w" : "l"), len, &opr_src, &opr_dest);
+    operand_read(&opr_src);
+	operand_read(&opr_dest);
+	alu_sub(opr_src.val, opr_dest.val, data_size);
+	return len;
+}
