ea8960ef95d8ccf28127e5778488f25a2d746ec6
tracer-pa-public <tracer@njuics.org>
2021-10-26 20:20:53 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:20:53 up 61 days,  9:25,  2 users,  load average: 2.08, 2.33, 2.55 > unitime > 31b4d2b0ebcded73965268244eb9a10b854d4387

--- nemu/src/cpu/instr/sbb.c
+++ nemu/src/cpu/instr/sbb.c
@@ -2,3 +2,10 @@
 /*
 Put the implementations of `sbb' instructions here.
 */
+static void instr_execute_2op() 
+{
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	opr_dest.val = alu_ssb(sign_ext(opr_src.val, opr_src.data_size), opr_dest.val, data_size);
+	operand_write(&opr_dest);
+}
