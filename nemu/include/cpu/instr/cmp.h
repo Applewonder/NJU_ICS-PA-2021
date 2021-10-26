851c6884f7f90fe39d90a26035f838c439015955
tracer-pa-public <tracer@njuics.org>
2021-10-26 08:38:46 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 08:38:46 up 60 days, 21:43,  2 users,  load average: 2.48, 2.42, 2.33 > unitime 1635208726077 > 18385d71e1b1a5c6faa3a3e851960623bacf8e94

--- nemu/include/cpu/instr/cmp.h
+++ nemu/include/cpu/instr/cmp.h
@@ -5,4 +5,5 @@ Put the declarations of `cmp' instructions here.
 */
 make_instr_func(cmp_i2rm_bv);
 make_instr_func(cmp_r2rm_v);
+make_instr_func(cmp_i2rm_v);
 #endif
--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -10,3 +10,4 @@ static void instr_execute_2op()
 }
 make_instr_impl_2op(cmp, i, rm, bv)
 make_instr_impl_2op(cmp, r, rm, v)
+make_instr_impl_2op(cmp, i, rm, v)
