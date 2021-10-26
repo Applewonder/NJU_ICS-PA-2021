1f633a94bf8f1b22730d8b48bc2eae2de429e560
tracer-pa-public <tracer@njuics.org>
2021-10-27 00:03:34 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 00:03:34 up 61 days, 13:07,  2 users,  load average: 2.18, 2.41, 2.42 > unitime 1635264214092 > 6f1d6e655e219f0584bc4082e8f530aa2a8c26b

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -6,32 +6,32 @@ static void instr_execute_2op()
 {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
-	opr_dest.val = alu_and(opr_src.val, opr_dest.val, data_size);
+	opr_dest.val = alu_and(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), 32);
 	cpu.eflags.CF = 0;
 	cpu.eflags.OF = 0;
 	operand_write(&opr_dest);
 }
 make_instr_impl_2op(test, r, rm, v)
 make_instr_impl_2op(test, r, rm, b)
-
+make_instr_impl_2op(test, i, rm, v)
 make_instr_impl_2op(test, i, a, b)
 make_instr_impl_2op(test, i, a, v)
 
-int test_i2rm_v(uint32_t eip, uint8_t opcode)
-{
-    int len = 1;
-    decode_data_size_v;
-    decode_operand_i2rm;
-    operand_read(&opr_src);
-	operand_read(&opr_dest);
-	opr_dest.val = alu_and(opr_src.val, opr_dest.val, data_size);
-	cpu.eflags.CF = 0;
-	cpu.eflags.OF = 0;
-	operand_write(&opr_dest);
-    if (data_size == 16) {
-        len = 4;
-    }else if(data_size == 32) {
-	    len = 6;
-    }
-    return len;
-}
+// int test_i2rm_v(uint32_t eip, uint8_t opcode)
+// {
+//     int len = 1;
+//     decode_data_size_v;
+//     decode_operand_i2rm;
+//     operand_read(&opr_src);
+// 	operand_read(&opr_dest);
+// 	opr_dest.val = alu_and(opr_src.val, opr_dest.val, data_size);
+// 	cpu.eflags.CF = 0;
+// 	cpu.eflags.OF = 0;
+// 	operand_write(&opr_dest);
+//     if (data_size == 16) {
+//         len = 4;
+//     }else if(data_size == 32) {
+// 	    len = 6;
+//     }
+//     return len;
+// }
