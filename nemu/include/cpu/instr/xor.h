3d8dce6227f239830087e2965050c7d2f53b29a2
tracer-pa-public <tracer@njuics.org>
2021-10-27 18:56:04 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:56:04 up 62 days,  8:00,  2 users,  load average: 2.59, 2.46, 2.40 > unitime 1635332163975 > 57e491b633c0393ae8aafbf1b805f31f56b97d8a

--- nemu/include/cpu/instr/xor.h
+++ nemu/include/cpu/instr/xor.h
@@ -7,4 +7,9 @@ make_instr_func(xor_r2rm_v);
 make_instr_func(xor_i2rm_b);
 make_instr_func(xor_i2rm_v);
 make_instr_func(xor_i2rm_bv);
+make_instr_func(xor_i2a_b);
+make_instr_func(xor_i2a_v);
+make_instr_func(xor_r2rm_b);
+make_instr_func(xor_rm2r_v);
+make_instr_func(xor_rm2r_b);
 #endif
