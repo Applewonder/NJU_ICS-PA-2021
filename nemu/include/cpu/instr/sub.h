35df152caa50863671a8570b47b9206b6e962773
tracer-pa-public <tracer@njuics.org>
2021-10-27 18:53:14 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:53:14 up 62 days,  7:57,  2 users,  load average: 2.44, 2.40, 2.38 > unitime 1635331994802 > 4c43b61d2e633d1a2c739104f2365abc736a7f0

--- nemu/include/cpu/instr/sub.h
+++ nemu/include/cpu/instr/sub.h
@@ -8,4 +8,8 @@ make_instr_func(sub_rm2r_v);
 make_instr_func(sub_r2rm_v);
 make_instr_func(sub_i2rm_v);
 make_instr_func(sub_i2rm_b);
+make_instr_func(sub_rm2r_b);
+make_instr_func(sub_r2rm_b);
+make_instr_func(sub_i2a_v);
+make_instr_func(sub_i2a_b);
 #endif
