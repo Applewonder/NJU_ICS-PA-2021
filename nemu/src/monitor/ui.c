ca3c9a92194ba83a050c810e70dcb590d47ab44d
tracer-pa-public <tracer@njuics.org>
2021-11-23 21:49:45 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:49:45 up 89 days, 10:54,  3 users,  load average: 2.37, 2.48, 2.49 > unitime 1637675385364 > ca9ffa751ccd3545fef230f5b79e3dd4566148a0

--- nemu/src/monitor/ui.c
+++ nemu/src/monitor/ui.c
@@ -98,7 +98,7 @@ cmd_handler(cmd_p)
 	}
 	//if(args + strspn(args, " ") >= cmd_end) { goto p_error; }
 
-	bool success;
+	bool success = true;
 	uint32_t val = expr(args, &success);
 	if (!success)
 	{
