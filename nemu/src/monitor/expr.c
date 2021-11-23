f68d982291076915e61ae56abdac85d91f9a56ed
tracer-pa-public <tracer@njuics.org>
2021-11-23 16:26:18 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:26:18 up 89 days,  5:30,  3 users,  load average: 2.78, 2.44, 2.38 > unitime 1637655978339 > cd9cd0454f90a7c3a1056684f372a5e16ee0950f

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -354,7 +354,7 @@ uint32_t expr(char *e, bool *success)
 	    }
 	}
 	for (int i = 0; i < nr_token; i++) {
-	    printf("%d", tokens[i].type);
+	    printf("%d\n", tokens[i].type);
 	}
     uint32_t result = eval(0, nr_token, success);
     return result;
