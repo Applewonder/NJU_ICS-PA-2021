cb1449f712d08cff524475d121813ab8012f5fa6
tracer-pa-public <tracer@njuics.org>
2021-12-02 13:24:08 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 13:24:08 up 98 days,  2:28,  3 users,  load average: 2.33, 2.41, 2.49 > unitime 1638422648680 > abb52e01a81976f3288a643958cd2e25ba4a9289

--- nemu/src/memory/mmu/cache.c
+++ nemu/src/memory/mmu/cache.c
@@ -78,7 +78,7 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data)
 	    uint32_t ias = ((paddr + 64 - caddr) >> 6) % 128;
 	    uint32_t it = (paddr + 64 - caddr) >> 13;
 	    uint32_t icaddr = (paddr + 64 -caddr) % 64;
-	    assert(0);
+	    printf("ias = %ud, it = %ud," ias, it);
 	    uint32_t iloc = locate_cache(suc, ias, it);
 	    if(suc) {
 	        
