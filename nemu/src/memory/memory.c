b03924cec7afb1bd0789176a6cf067bdc7ebc05b
tracer-pa-public <tracer@njuics.org>
2021-12-03 00:11:07 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 00:11:07 up 98 days, 13:15,  3 users,  load average: 2.65, 2.64, 2.71 > unitime 1638461466952 > 3b042b6c9528942b89789960ad79b883cc631c0

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -24,7 +24,7 @@ uint32_t paddr_read(paddr_t paddr, size_t len)
 	uint32_t ret = 0;
 #ifdef CACHE_ENABLED
 		ret = cache_read(paddr, len);
-		assert(ret == hw_mem_read(paddr, len));
+		\\assert(ret == hw_mem_read(paddr, len));
 		// 通过cache进行读
 #else
 	    ret = hw_mem_read(paddr, len);
