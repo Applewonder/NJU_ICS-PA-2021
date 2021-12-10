d56d2e047d037b9e3bfd16ba84f71bfca5fdd9e7
tracer-pa-public <tracer@njuics.org>
2021-12-10 23:42:13 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:42:13 up 106 days, 12:46,  3 users,  load average: 2.38, 2.58, 2.60 > unitime 1639150933024 > c5beeaec00bbf553ced7bebf860d612b8af00c0

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -5,13 +5,13 @@ Put the implementations of `lgdt' instructions here.
 make_instr_func(lgdt)
 {
     uint32_t addr = vaddr_read(eip + 3, 1, 4);
-    assert(0);
     OPERAND opr;
     opr.type = OPR_MEM;
     opr.addr = addr;
     opr.sreg = SREG_DS;
     opr.data_size = 16;
     operand_read(&opr);
+    assert(0);
     cpu.gdtr.limit = opr.val;
     opr.addr += 2;
     opr.data_size = 32;
