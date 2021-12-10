10a5d2599b2a0a3fb7d7d61ed8e1a21de9cf45ca
tracer-pa-public <tracer@njuics.org>
2021-12-10 17:08:17 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:08:17 up 106 days,  6:12,  3 users,  load average: 2.69, 2.69, 2.64 > unitime 1639127297377 > 4593606ebc83b1124528cb0c145aee92a6f299e

--- include/config.h
+++ include/config.h
@@ -4,8 +4,8 @@
 // #define NEMU_REF_INSTR
 
 // PA 3
- #define CACHE_ENABLED
-//#define IA32_SEG             // protect mode enable
+#define CACHE_ENABLED
+#define IA32_SEG             // protect mode enable
 //#define IA32_PAGE            // virtual memory management is now complete
 //#define TLB_ENABLED
 
