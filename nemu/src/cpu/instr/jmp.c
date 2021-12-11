affd0d71435a94b977849775bf9bc5aa7c395e4c
tracer-pa-public <tracer@njuics.org>
2021-12-11 11:28:49 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:28:49 up 107 days, 33 min,  3 users,  load average: 2.61, 2.46, 2.50 > unitime 1639193329199 > fa27c0862bd5fb5fcb2ac65490b8cd5d4c9f6c8

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -47,7 +47,7 @@ make_instr_func(jmp_near_indirect)
 
 make_instr_func(jmp_far_imm)
 {
-        cpu.cs = (1 << 3);
+        cpu.segReg[1] = (1 << 3);
         OPERAND reg;
         reg.data_size = 32;
         reg.type = OPR_IMM;
