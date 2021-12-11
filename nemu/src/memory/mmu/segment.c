5fc054bc0f20bce9ad03754834b2d0acff783b5b
tracer-pa-public <tracer@njuics.org>
2021-12-11 22:17:00 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:17:00 up 107 days, 11:21,  3 users,  load average: 2.37, 2.46, 2.50 > unitime 1639232220368 > 1360526da6e8150081f3fe04e3e4ff343613c201

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -27,7 +27,7 @@ void load_sreg(uint8_t sreg)
     seg.val[1] = vaddr_read(cpu.gdtr.base + cpu.segReg[sreg].index * 8 + 32, sreg, 4);
     //printf("%x\n", seg.val[0]);
     //printf("%x\n", seg.val[1]);
-    assert(seg.granularity == 1);
+    //assert(seg.granularity == 1);
     cpu.segReg[sreg].base = (seg.base_31_24 << 24) + (seg.base_23_16 << 16) + seg.base_15_0;
     cpu.segReg[sreg].limit = (seg.limit_19_16 << 16) + seg.limit_15_0;
     cpu.segReg[sreg].type = seg.type;
