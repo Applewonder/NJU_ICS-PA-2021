24bbcd117a1e182af1310d2077d8d2ff2c71607d
tracer-pa-public <tracer@njuics.org>
2021-12-11 11:50:00 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:50:00 up 107 days, 54 min,  3 users,  load average: 2.12, 2.50, 2.56 > unitime > 8ac46002e8ac17cc6ef83bc03399feff973d607e

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -76,3 +76,17 @@ make_instr_func(mov_srm162r_l) {
 	print_asm_2("mov", "", len, &rm, &r);
         return len;
 }
+
+make_instr_func(mov_rm2s_w) {
+        int len = 1;
+        OPERAND rm, s;
+        rm.data_size = 16;
+        rm.data_size = 16;
+        len += modrm_r_rm(eip + 1, &r, &rm);
+        operand_read(&rm);
+        r.val = sign_ext(rm.val, 16);
+        operand_write(&r);
+
+	print_asm_2("mov", "", len, &rm, &r);
+        return len;
+}
