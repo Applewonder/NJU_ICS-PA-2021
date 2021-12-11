d17ec3e558f56656a6e8e7994a9318d62013e4f1
tracer-pa-public <tracer@njuics.org>
2021-12-11 18:00:24 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:00:24 up 107 days,  7:04,  3 users,  load average: 2.39, 2.55, 2.57 > unitime 1639216824175 > 820e8dfc514bb2e4316db49991c52d7a5e856d5e

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -7,7 +7,7 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg)
 	/* TODO: perform segment translation from virtual address to linear address
 	 * by reading the invisible part of the segment register 'sreg'
 	 */
-	uint32_t res = segReg[sreg].base + offset;
+	uint32_t res = cpu.segReg[sreg].base + offset;
 	return res;
 }
 
@@ -17,9 +17,9 @@ void load_sreg(uint8_t sreg)
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
-	 segReg[sreg].base = (cpu.gdtr.base[segReg[sreg].index].base_31_24 << 24) + (cpu.gdtr.base[segReg[sreg].index].base_23_16 << 16) + cpu.gdtr.base[segReg[sreg].index].base_15_0;
-	 segReg[sreg].limit = (cpu.gdtr.base[segReg[sreg].index].limit_19_16 << 16) + cpu.gdtr.base[segReg[sreg].index].limit_15_0;
-	 segReg[sreg].type = cpu.gdtr.base[segReg[sreg].index].type;
-	 segReg[sreg].privilege_level = cpu.gdtr.base[segReg[sreg].index].privilege_level;
-	 segReg[sreg].soft_use = cpu.gdtr.base[segReg[sreg].index].soft_use;
+	 cpu.segReg[sreg].base = (cpu.gdtr.base[segReg[sreg].index].base_31_24 << 24) + (cpu.gdtr.base[segReg[sreg].index].base_23_16 << 16) + cpu.gdtr.base[segReg[sreg].index].base_15_0;
+	 cpu.segReg[sreg].limit = (cpu.gdtr.base[segReg[sreg].index].limit_19_16 << 16) + cpu.gdtr.base[segReg[sreg].index].limit_15_0;
+	 cpu.segReg[sreg].type = cpu.gdtr.base[segReg[sreg].index].type;
+	 cpu.segReg[sreg].privilege_level = cpu.gdtr.base[segReg[sreg].index].privilege_level;
+	 cpu.segReg[sreg].soft_use = cpu.gdtr.base[segReg[sreg].index].soft_use;
 }
