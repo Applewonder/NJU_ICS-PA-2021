7c48d04f528ab2e70373ed387202bd93e1f0508f
tracer-pa-public <tracer@njuics.org>
2021-12-11 22:38:20 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:38:20 up 107 days, 11:42,  3 users,  load average: 2.51, 2.38, 2.44 > unitime 1639233500187 > b044fcdc1d4f78e15d0bbd66c47e7c80c79f9278

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -55,13 +55,30 @@ void laddr_write(laddr_t laddr, size_t len, uint32_t data)
 uint32_t vaddr_read(vaddr_t vaddr, uint8_t sreg, size_t len)
 {
 	assert(len == 1 || len == 2 || len == 4);
+#ifndef IA32_SEG
 	return laddr_read(vaddr, len);
+#else
+	uint32_t laddr = vaddr;
+	if(cpu.cr0.pe == 1) {
+		laddr = segment_translate(vaddr, sreg);
+	}
+	return laddr_read(laddr, len);
+#endif
 }
 
 void vaddr_write(vaddr_t vaddr, uint8_t sreg, size_t len, uint32_t data)
 {
 	assert(len == 1 || len == 2 || len == 4);
+#ifndef IA32_SEG
 	laddr_write(vaddr, len, data);
+#else
+	uint32_t laddr = vaddr;
+	if(cpu.cr0.pe == 1) {
+		laddr = segment_translate(vaddr, sreg);
+	}
+	laddr_write(vaddr, len, data);
+#endif
+	
 }
 
 void init_mem()
--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -25,15 +25,9 @@ void load_sreg(uint8_t sreg)
     SegDesc seg;
     seg.val[0] = vaddr_read(cpu.gdtr.base + cpu.segReg[sreg].index * 8, sreg, 4);
     seg.val[1] = vaddr_read(cpu.gdtr.base + cpu.segReg[sreg].index * 8 + 4, sreg, 4);
-    printf("index = %x\n", cpu.segReg[sreg].index);
-    printf("granularity = %x, reg.base = %x, %x, %x, val[1] = %x, val[2]%x\n", seg.granularity, seg.base_31_24, seg.base_23_16, seg.base_15_0, seg.val[0], seg.val[1]);
-    printf("limit_19_16 = %x, limit_15_0 = %x", seg.limit_19_16, seg.limit_15_0);
-    assert(seg.granularity == 1);
     cpu.segReg[sreg].base = (seg.base_31_24 << 24) + (seg.base_23_16 << 16) + seg.base_15_0;
     cpu.segReg[sreg].limit = (seg.limit_19_16 << 16) + seg.limit_15_0;
     cpu.segReg[sreg].type = seg.type;
     cpu.segReg[sreg].privilege_level = seg.privilege_level;
     cpu.segReg[sreg].soft_use = seg.soft_use;
-    assert(cpu.segReg[sreg].base == 0);
-    assert(cpu.segReg[sreg].limit == 0xfffff);
  }
