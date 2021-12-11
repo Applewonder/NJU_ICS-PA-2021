0cad32d01caa178dde86a2d067ec77624bd5bfde
tracer-pa-public <tracer@njuics.org>
2021-12-11 22:40:03 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:40:03 up 107 days, 11:44,  3 users,  load average: 2.52, 2.43, 2.45 > unitime 1639233603568 > f7d463b6433573f2984e9d26f9f086aa904bfa66

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -6,25 +6,17 @@ make_instr_func(lgdt)
 {
     int len = 1;
     OPERAND opr;
-    //printf("%x\n", eip);
     opr.type = OPR_MEM;
-    //opr.addr = 0x0003004c;
     len += modrm_rm(eip + 1, &opr);
-    //assert(opr.addr == 0x0003004c);
     opr.sreg = SREG_DS;
     opr.data_size = 16;
-    //assert(0);
     operand_read(&opr);
     
-    //assert(0);
     cpu.gdtr.limit = opr.val;
-    printf("limit = %x\n", cpu.gdtr.limit);
     opr.addr += 2;
     opr.data_size = 32;
     
     operand_read(&opr);
-    printf("base = %x\n", opr.val);
-    //assert(0);
     cpu.gdtr.base = opr.val;
     return len;
 }
