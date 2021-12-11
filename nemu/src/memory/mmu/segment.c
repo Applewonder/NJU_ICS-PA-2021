62aeaecf93e808c591f7c33830535653e74723d2
tracer-pa-public <tracer@njuics.org>
2021-12-11 18:19:24 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:19:24 up 107 days,  7:23,  3 users,  load average: 3.34, 2.82, 2.70 > unitime 1639217964077 > b1c1abc634e567005dca0022ebb36106cddee7c

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -17,9 +17,9 @@ void load_sreg(uint8_t sreg)
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
-	 cpu.segReg[sreg].base = ((cpu.gdtr.base + cpu.segReg[sreg].index * 8).base_31_24 << 24) + (cpu.gdtr.base[cpu.segReg[sreg].index].base_23_16 << 16) + cpu.gdtr.base[cpu.segReg[sreg].index].base_15_0;
-	 cpu.segReg[sreg].limit = (cpu.gdtr.base[cpu.segReg[sreg].index].limit_19_16 << 16) + cpu.gdtr.base[cpu.segReg[sreg].index].limit_15_0;
-	 cpu.segReg[sreg].type = cpu.gdtr.base[cpu.segReg[sreg].index].type;
-	 cpu.segReg[sreg].privilege_level = cpu.gdtr.base[cpu.segReg[sreg].index].privilege_level;
-	 cpu.segReg[sreg].soft_use = cpu.gdtr.base[cpu.segReg[sreg].index].soft_use;
-}
+// 	 cpu.segReg[sreg].base = ((cpu.gdtr.base + cpu.segReg[sreg].index * 8).base_31_24 << 24) + (cpu.gdtr.base[cpu.segReg[sreg].index].base_23_16 << 16) + cpu.gdtr.base[cpu.segReg[sreg].index].base_15_0;
+// 	 cpu.segReg[sreg].limit = (cpu.gdtr.base[cpu.segReg[sreg].index].limit_19_16 << 16) + cpu.gdtr.base[cpu.segReg[sreg].index].limit_15_0;
+// 	 cpu.segReg[sreg].type = cpu.gdtr.base[cpu.segReg[sreg].index].type;
+// 	 cpu.segReg[sreg].privilege_level = cpu.gdtr.base[cpu.segReg[sreg].index].privilege_level;
+// 	 cpu.segReg[sreg].soft_use = cpu.gdtr.base[cpu.segReg[sreg].index].soft_use;
+ }
