72cc52d40a34e07c65a110d0d6802d78775c49b9
tracer-pa-public <tracer@njuics.org>
2021-12-11 22:26:29 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:26:29 up 107 days, 11:30,  3 users,  load average: 2.86, 2.66, 2.58 > unitime 1639232789753 > 540bc0f74a210a9c376291f63e788001840dd543

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -26,9 +26,8 @@ void load_sreg(uint8_t sreg)
     seg.val[1] = vaddr_read(cpu.gdtr.base + cpu.segReg[sreg].index * 8, sreg, 4);
     seg.val[0] = vaddr_read(cpu.gdtr.base + cpu.segReg[sreg].index * 8 + 32, sreg, 4);
     printf("index = %x\n", cpu.segReg[sreg].index);
-    printf("granularity = %x\n", seg.granularity);
-    printf("reg.base = %x\n, %x\n, %x\n", seg.base_31_24, seg.base_23_16, seg.base_15_0);
-    printf("%x\n", seg.val[0]);
+    printf("granularity = %x, reg.base = %x, %x, %x", seg.granularity, seg.base_31_24, seg.base_23_16, seg.base_15_0);
+    printf("val[1] = %x\n", seg.val[0]);
     printf("%x\n", seg.val[1]);
     //assert(seg.granularity == 1);
     cpu.segReg[sreg].base = (seg.base_31_24 << 24) + (seg.base_23_16 << 16) + seg.base_15_0;
