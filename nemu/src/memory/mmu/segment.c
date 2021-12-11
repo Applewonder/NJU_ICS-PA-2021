5a8eae2a35017aa0a5393382638aa0b8acc1ce30
tracer-pa-public <tracer@njuics.org>
2021-12-11 22:29:33 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:29:33 up 107 days, 11:33,  3 users,  load average: 2.41, 2.53, 2.54 > unitime 1639232973278 > 760372635e5370c8a75a0862563cb8bb3ae1556

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -27,12 +27,12 @@ void load_sreg(uint8_t sreg)
     seg.val[1] = vaddr_read(cpu.gdtr.base + cpu.segReg[sreg].index * 8 + 4, sreg, 4);
     printf("index = %x\n", cpu.segReg[sreg].index);
     printf("granularity = %x, reg.base = %x, %x, %x, val[1] = %x, val[2]%x\n", seg.granularity, seg.base_31_24, seg.base_23_16, seg.base_15_0, seg.val[0], seg.val[1]);
-    //assert(seg.granularity == 1);
+    assert(seg.granularity == 1);
     cpu.segReg[sreg].base = (seg.base_31_24 << 24) + (seg.base_23_16 << 16) + seg.base_15_0;
     cpu.segReg[sreg].limit = (seg.limit_19_16 << 16) + seg.limit_15_0;
     cpu.segReg[sreg].type = seg.type;
     cpu.segReg[sreg].privilege_level = seg.privilege_level;
     cpu.segReg[sreg].soft_use = seg.soft_use;
-    //assert(cpu.segReg[sreg].base == 0);
-    //assert(cpu.segReg[sreg].limit == 0xffffffff);
+    assert(cpu.segReg[sreg].base == 0);
+    assert(cpu.segReg[sreg].limit == 0xffffffff);
  }
