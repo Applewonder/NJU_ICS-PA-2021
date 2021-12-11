0b6d1f6c8b4dd2f57f175a88f8bb321c419de157
tracer-pa-public <tracer@njuics.org>
2021-12-11 21:21:09 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:21:09 up 107 days, 10:25,  3 users,  load average: 2.79, 2.44, 2.42 > unitime 1639228869266 > cb5b2c457cb97a0bd0b2a03ef58e575a79375116

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -13,7 +13,7 @@ make_instr_func(lgdt)
     //assert(opr.addr == 0x0003004c);
     opr.sreg = SREG_DS;
     opr.data_size = 16;
-    //assert(0);
+    assert(0);
     operand_read(&opr);
     
     //assert(0);
