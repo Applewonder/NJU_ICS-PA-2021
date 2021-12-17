85599a28868c90849c68bb4a32e0f925c032cda0
tracer-pa-public <tracer@njuics.org>
2021-12-17 22:41:32 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:41:32 up 113 days, 11:45,  3 users,  load average: 2.53, 2.57, 2.60 > unitime > b40cdc83b800bfbb56bd079dfe618afae2cbf46a

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -10,10 +10,11 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t offset = laddr % 12;
 	uint32_t m = (cpu.cr3.pdbr << 12) + 8*dir;
 	PDE t;
-	memcpy(&t, &m, 4);
+	t.val = hw_mem_read(m, 4);
 	assert(t.present == 1);
 	PTE l;
-	memcpy(&l, (t.page_frame << 12) + 8*page, 4);
+	m = (t.page_frame << 12) + 8*page;
+	l.val = hw_mem_read(m, 4);
 	assert(l.present == 1);
 	paddr_t result = (l.page_frame << 12) + offset;
 	return result;
