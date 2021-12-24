d84e0c84ab2fd7847295fe524fd5ebc830f92f16
tracer-pa-public <tracer@njuics.org>
2021-12-24 21:49:58 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 21:49:58 up 120 days, 10:54,  3 users,  load average: 2.45, 2.40, 2.47 > unitime > 924e82da624d4066c6b0601ce5c97c33a6c4c6bc

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -5,38 +5,38 @@
 paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
-	uint32_t dir = laddr >> 22;
-	uint32_t page = (laddr >> 12) % 1024;
-	uint32_t offset = laddr % 4096;
-	uint32_t m = (cpu.cr3.pdbr << 12) + 4*dir;
-	PDE t;
-	printf("laddr = -------->%x\n", laddr);
-	fflush(stdout);
-	printf("page = -------->%x\n", page);
-	fflush(stdout);
+// 	uint32_t dir = laddr >> 22;
+// 	uint32_t page = (laddr >> 12) % 1024;
+// 	uint32_t offset = laddr % 4096;
+// 	uint32_t m = (cpu.cr3.pdbr << 12) + 4*dir;
+// 	PDE t;
+// 	printf("laddr = -------->%x\n", laddr);
+// 	fflush(stdout);
+// 	printf("page = -------->%x\n", page);
+// 	fflush(stdout);
 	
-	memcpy(&t.val, hw_mem + m, 4);
+// 	memcpy(&t.val, hw_mem + m, 4);
 	
-	assert(t.present == 1);
-	PTE l;
+// 	assert(t.present == 1);
+// 	PTE l;
 	
 	
-//	m = (t.page_frame << 12) + 4*page;
+// //	m = (t.page_frame << 12) + 4*page;
 	
-	printf("m = -------->%x\n", m);
-	fflush(stdout);
-	printf("t.page_frame-------->%x\n", t.page_frame);
-	fflush(stdout);
+// 	printf("m = -------->%x\n", m);
+// 	fflush(stdout);
+// 	printf("t.page_frame-------->%x\n", t.page_frame);
+// 	fflush(stdout);
 	
-	memcpy(&l.val, hw_mem + m, 4);
+// 	memcpy(&l.val, hw_mem + m, 4);
 
-	//assert(0);
-//	printf("-------->%d", l.present);
-// 	assert(l.present == 1);
- //	assert(0);
-	paddr_t result = (l.page_frame << 12) + offset;
-// 	assert(0);
-	return result;
+// 	//assert(0);
+// //	printf("-------->%d", l.present);
+// // 	assert(l.present == 1);
+//  //	assert(0);
+// 	paddr_t result = (l.page_frame << 12) + offset;
+// // 	assert(0);
+// 	return result;
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
 #endif
