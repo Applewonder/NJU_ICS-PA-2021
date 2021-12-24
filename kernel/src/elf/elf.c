17e483e51452ed50d13e51bd6a2ecac96b07fffa
tracer-pa-public <tracer@njuics.org>
2021-12-24 15:43:22 +0800

    > test_pa-3-3 > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:43:22 up 120 days,  4:47,  3 users,  load average: 3.52, 2.68, 2.53 > unitime 1640331789987 > f365f3aece9a50f1674ad8f1ae56119ffdc710ec

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -32,14 +32,14 @@ uint32_t loader()
 	/* Load each program segment */
 	ph = (void *)elf + elf->e_phoff;
 	eph = ph + elf->e_phnum;
-	
+	uint32_t prog_start = mm_malloc(ph->p_vaddr, ph->p_memsz);
 	for (; ph < eph; ph++)
 	{
 		if (ph->p_type == PT_LOAD)
 		{
 
 			// remove this panic!!!
-			uint32_t prog_start = mm_malloc(ph->p_vaddr, ph->p_memsz);
+			
 /* TODO: copy the segment from the ELF file to its proper memory area */
             memcpy((void *)elf + prog_start, (void *)elf + ph->p_offset, ph->p_filesz);
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
