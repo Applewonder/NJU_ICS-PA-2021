15882eedf5c71cf227fbdb78bd7a1b987e9b0a0b
tracer-pa-public <tracer@njuics.org>
2021-12-25 11:00:04 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:00:04 up 121 days, 4 min,  3 users,  load average: 2.55, 2.68, 2.62 > unitime 1640401204354 > e8a8054fcaba60ea12b36b5e4614c21b9b7df92f

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -7,8 +7,8 @@
 typedef union {
         struct
         {
-            uint32_t pdbr : 20;
             uint32_t reserved : 12;
+            uint32_t pdbr : 20;
         };
         uint32_t val;
     }CR3;
