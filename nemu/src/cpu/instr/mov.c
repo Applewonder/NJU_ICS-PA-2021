a7bf248ff73e4d47da404633bbb33c53acf5b97b
tracer-pa-public <tracer@njuics.org>
2021-12-25 18:36:13 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 18:36:13 up 121 days,  7:40,  3 users,  load average: 3.22, 2.90, 2.64 > unitime 1640428573644 > ae169aec8167765f4d41eacd56abaa3297e921b9

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -100,6 +100,7 @@ make_instr_func(mov_r2c_l) {
         OPERAND r, c;
         r.data_size = 32;
         c.data_size = 32;
+        c.type = OPR_CREG;
         len += modrm_r_rm(eip + 1,  &r, &c);
         operand_read(&r);
         c.val = r.val;
@@ -114,6 +115,7 @@ make_instr_func(mov_c2r_l) {
         r.data_size = 32;
         c.data_size = 32;
         len += modrm_r_rm(eip + 1, &r, &c);
+        c.type = OPR_CREG;
         operand_read(&c);
         r.val = c.val;
         operand_write(&r);
