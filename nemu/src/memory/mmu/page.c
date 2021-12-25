939c1f2b40bf44723583c371b7c3417a64409aaf
tracer-pa-public <tracer@njuics.org>
2021-12-25 10:41:46 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:41:46 up 120 days, 23:46,  3 users,  load average: 2.68, 2.66, 2.58 > unitime 1640400106141 > c8dea9315e7c22747d1a0dd1502d6cdd4dd4a4e0

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -39,7 +39,7 @@ paddr_t page_translate(laddr_t laddr)
 //	return result;
     uint32_t a = (uint32_t)hw_mem + (cpu.cr3.pdbr<<12) + 4*(laddr >> 22);
     PDE* b = (PDE*) a;
-    assert(b->present == 1);
+  //  assert(b->present == 1);
     a = (uint32_t)hw_mem + 4*((laddr >> 12)&0x3ff) + (b -> page_frame << 12);
     PTE* res = (PTE*)a;
     assert(res->present == 1);
