81b2abcaf43c29dab92ef1f68a2cce52199952aa
tracer-pa-public <tracer@njuics.org>
2021-12-25 14:45:19 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:45:19 up 121 days,  3:49,  3 users,  load average: 2.84, 2.63, 2.58 > unitime > c637cf4457093fad611356a9fcb24a6f3b5e38f9

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -5,62 +5,62 @@
 paddr_t page_translate(laddr_t laddr)
 {
  #ifndef TLB_ENABLED
-	uint32_t dir = laddr >> 22;
-	uint32_t page = (laddr >> 12) % 1024;
-	uint32_t offset = laddr % 4096;
-	uint32_t m = (cpu.cr3.pdbr << 12) + 4*dir;
-	PDE t;
-	printf("laddr = -------->%x\n", laddr);
-	fflush(stdout);
-	printf("page = -------->%x\n", page);
-	fflush(stdout);
-	printf("m = -------->%x\n", m);
-	fflush(stdout);
-	printf("offset-------->%x\n", offset);
-	fflush(stdout);
+// 	uint32_t dir = laddr >> 22;
+// 	uint32_t page = (laddr >> 12) % 1024;
+// 	uint32_t offset = laddr % 4096;
+// 	uint32_t m = (cpu.cr3.pdbr << 12) + 4*dir;
+// 	PDE t;
+// // 	printf("laddr = -------->%x\n", laddr);
+// // 	fflush(stdout);
+// // 	printf("page = -------->%x\n", page);
+// // 	fflush(stdout);
+// // 	printf("m = -------->%x\n", m);
+// // 	fflush(stdout);
+// // 	printf("offset-------->%x\n", offset);
+// // 	fflush(stdout);
 	
-	memcpy(&t.val, hw_mem + m, 4); //这里报seg fault
-	//assert(0);//这里加断点
-	assert(t.present);
-	PTE l;
+// 	memcpy(&t.val, hw_mem + m, 4); //这里报seg fault
+// 	//assert(0);//这里加断点
+// 	assert(t.present);
+// 	PTE l;
 	
-	m = (t.page_frame << 12) + 4*page;
+// 	m = (t.page_frame << 12) + 4*page;
 	
-	printf("m = -------->%x\n", m);
-	fflush(stdout);
-	printf("t.page_frame-------->%x\n", t.page_frame);
-	fflush(stdout);
-	memcpy(&l.val, hw_mem + m, 4);
+// // 	printf("m = -------->%x\n", m);
+// // 	fflush(stdout);
+// // 	printf("t.page_frame-------->%x\n", t.page_frame);
+// // 	fflush(stdout);
+// 	memcpy(&l.val, hw_mem + m, 4);
 
-	//assert(0);
-	printf("-------->%d", l.present);
-	assert(l.present == 1);
- //	assert(0);
-	paddr_t result = (l.page_frame << 12) + offset;
-	return result;
-//     printf("------>%x\n", laddr);
-//     fflush(stdout);
-//     printf("------>%d\n", cpu.cr3.pdbr);;
-//     fflush(stdout);
+// 	//assert(0);
+// // 	printf("-------->%d", l.present);
+// // 	assert(l.present == 1);
+//  //	assert(0);
+// 	paddr_t result = (l.page_frame << 12) + offset;
+// 	return result;
+    printf("------>%x\n", laddr);
+    fflush(stdout);
+    printf("------>%d\n", cpu.cr3.pdbr);;
+    fflush(stdout);
     
-//     uint32_t a = (uint32_t)hw_mem + (cpu.cr3.pdbr<<12) + 4*(laddr >> 22);
-//     printf("------>%x\n", a);
-//     fflush(stdout);
-//     printf("------>%x\n", 4*(laddr >> 22));
-//     fflush(stdout);
+    uint32_t a = (uint32_t)hw_mem + (cpu.cr3.pdbr<<12) + 4*(laddr >> 22);
+    printf("------>%x\n", a);
+    fflush(stdout);
+    printf("------>%x\n", 4*(laddr >> 22));
+    fflush(stdout);
     
-//     PDE* b = (PDE*) a;
-//     PDE t;
-//     memcpy(&t.val, hw_mem + a, 4); 
-//     printf("------>%x\n", t.val);
-//     fflush(stdout);
-//     printf("------>%x\n", a);
-//     fflush(stdout);
-//   //  assert(b->present == 1);
-//     a = (uint32_t)hw_mem + 4*((laddr >> 12)&0x3ff) + (b -> page_frame << 12);
-//     PTE* res = (PTE*)a;
-//     assert(res->present == 1);
-//     return (laddr&0xfff) + (res->page_frame << 12);
+    PDE* b = (PDE*) a;
+    PDE t;
+    memcpy(&t.val, hw_mem + a, 4); 
+    printf("------>%x\n", t.val);
+    fflush(stdout);
+    printf("------>%x\n", a);
+    fflush(stdout);
+  //  assert(b->present == 1);
+    a = (uint32_t)hw_mem + 4*((laddr >> 12)&0x3ff) + (b -> page_frame << 12);
+    PTE* res = (PTE*)a;
+    assert(res->present == 1);
+    return (laddr&0xfff) + (res->page_frame << 12);
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
 #endif
