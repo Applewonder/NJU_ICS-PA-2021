a63eac9d99c2f25ed95a6948bbd466366f3fefc4
tracer-pa-public <tracer@njuics.org>
2021-12-25 17:30:35 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:30:35 up 121 days,  6:34,  3 users,  load average: 2.13, 2.55, 2.58 > unitime > 3b9b426982b3ffa3da54c62493849384b4631431

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -6,23 +6,29 @@ Put the implementations of `pop' instructions here.
 static void instr_execute_1op()
 {
     
-    uint32_t opcode = instr_fetch(cpu.eip, 1);
-    opcode -= 0x58;
+    // uint32_t opcode = instr_fetch(cpu.eip, 1);
+    // opcode -= 0x58;
     
-    OPERAND esp;
-    esp.type = OPR_MEM;
-    esp.addr = cpu.esp;
-    esp.data_size = 32;
-    operand_read(&esp);
+    // OPERAND esp;
+    // esp.type = OPR_MEM;
+    // esp.addr = cpu.esp;
+    // esp.data_size = 32;
+    // operand_read(&esp);
     
-    OPERAND opr;
-    opr.type = OPR_REG;
-    opr.addr = opcode;
-    opr.data_size = 32;
-    opr.val = esp.val;
+    // OPERAND opr;
+    // opr.type = OPR_REG;
+    // opr.addr = opcode;
+    // opr.data_size = 32;
+    // opr.val = esp.val;
     
-    operand_write(&opr);
-    cpu.esp += 4;
+    // operand_write(&opr);
+    // cpu.esp += 4;
+    opr.dest.addr = cpu.esp;
+    opr.dest.type = OPR.MEM;
+    operand_read(&opr_dest);
+    opr_src.val = opr_dest.val;
+    operand_write(&opr_src);
+    cpu.esp += data_size/8;
     
     
 }
