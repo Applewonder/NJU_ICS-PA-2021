20c26adfa8e93875d3a6778aa813e7f83afcc544
tracer-pa-public <tracer@njuics.org>
2021-12-25 14:51:27 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux a959ccd70ef6 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 14:51:27 up 121 days,  3:55,  3 users,  load average: 2.54, 2.52, 2.54 > unitime 1640415086872 > 904bbf80bba64464e2dd2cf2f0c2be61e0d5ba0

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -38,25 +38,23 @@ paddr_t page_translate(laddr_t laddr)
 //  //	assert(0);
 // 	paddr_t result = (l.page_frame << 12) + offset;
 // 	return result;
-    printf("------>%x\n", laddr);
-    fflush(stdout);
-    printf("------>%d\n", cpu.cr3.pdbr);;
-    fflush(stdout);
+    // printf("------>%x\n", laddr);
+    // fflush(stdout);
+    // printf("------>%d\n", cpu.cr3.pdbr);;
+    // fflush(stdout);
     
     uint32_t a = (uint32_t)hw_mem + (cpu.cr3.pdbr<<12) + 4*(laddr >> 22);
-    printf("------>%x\n", a);
-    fflush(stdout);
-    printf("------>%x\n", 4*(laddr >> 22));
-    fflush(stdout);
+    // printf("------>%x\n", a);
+    // fflush(stdout);
+    // printf("------>%x\n", 4*(laddr >> 22));
+    // fflush(stdout);
     
     PDE* b = (PDE*) a;
-    PDE t;
-    memcpy(&t.val, hw_mem + a, 4); 
     printf("------>%x\n", t.val);
     fflush(stdout);
     printf("------>%x\n", a);
     fflush(stdout);
-  //  assert(b->present == 1);
+    assert(b->present == 1);
     a = (uint32_t)hw_mem + 4*((laddr >> 12)&0x3ff) + (b -> page_frame << 12);
     PTE* res = (PTE*)a;
     assert(res->present == 1);
