95fbd2189c1de0230f7c6e69f76fc380199d3dac
tracer-pa-public <tracer@njuics.org>
2022-01-24 01:33:02 +0800

    > compile nemu testcase kernel > id 201502009 > user pa201502009 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 01:33:02 up  1:01,  1 user,  load average: 0.35, 0.16, 0.05 > unitime 1642959182030 > 3fa217a715a285a3d912a8232d76ad91331f93b8

--- include/config.h
+++ include/config.h
@@ -11,7 +11,7 @@
 
 // PA 4
 #define IA32_INTR            // tells NEMU and Kernel that we are ready for handling exceptions(traps only) and interrupts
-//#define HAS_DEVICE_TIMER
+#define HAS_DEVICE_TIMER
 //#define HAS_DEVICE_SERIAL    // the disturbing 'nemu trap output:' tag will be removed
 //#define HAS_DEVICE_IDE       // the loader will load the elf from the hard disk instead of mem disk
 //#define HAS_DEVICE_KEYBOARD  // keyboard input
