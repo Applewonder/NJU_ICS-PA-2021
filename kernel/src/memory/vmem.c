0b90198684704506f6cbd8c21b89f30f043612be
tracer-pa-public <tracer@njuics.org>
2022-01-24 21:40:41 +0800

    > clean > id 201502009 > user pa201502009 > uname Linux icspa 4.19.0-13-686 #1 SMP Debian 4.19.160-2 (2020-11-28) i686 GNU/Linux > uptime 21:40:41 up 21:09,  1 user,  load average: 0.12, 0.22, 0.17 > unitime > 5507f130904d53c27bfa8bd74f479c4819b04b0b

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -7,22 +7,26 @@
 #define NR_PT ((SCR_SIZE + PT_SIZE - 1) / PT_SIZE) // number of page tables to cover the vmem
 
 PDE *get_updir();
-static PTE uptable[NR_PT] align_to_page;
+//PTE uptable[NR_PT] align_to_page;
 
 void create_video_mapping()
 {
-
+	PTE ptable[1024] align_to_page;
 	/* TODO: create an identical mapping from virtual memory area
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memeory area
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
 	PDE *updir = (PDE *)va_to_pa(get_updir());
-	updir[0].val = make_pde(va_to_pa(uptable));
-	for(uint32_t addrv = 0xa0; addrv < 0xb0; addrv++) {
-		uptable[addrv].val = make_pde(0xa0 << 12);
+	//updir[0].val = make_pde(va_to_pa(uptable));
+	//for(uint32_t addrv = 0xa0; addrv < 0xb0; addrv++) {
+		//uptable[addrv].val = make_pde(0xa0 << 12);
+	//}
+	int count = 0;
+	for(int i = 0xa0; count <= 0xf; ++count, ++i){
+		ptable[0xa0 + count].val = make_pde(i << 12);
 	}
-
+	updir[0].val = make_pde(va_to_pa(ptable));
 }
 
 void video_mapping_write_test()
